fffe124bd75b508dd4779e6a8b450449f2f65e1f
Paige Watson <paige.watson@outlook.com>
2018-11-27 08:47:56 -0800

    Initial Commit

---
+++ PowerSetCalculatorTests/CalcutlatorTests.cs
@@ -0,0 +1,21 @@
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace PowerSetCalculatorTests
+{
+    [TestClass]
+    public class CalcutlatorTests
+    {
+
+        [TestMethod, TestCategory("Unit")]
+        public void ShouldNAME()
+        {
+            //assign
+            
+            //act
+
+            //assert
+
+        }
+    }
+}
