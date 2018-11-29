8b9282e816c9b0ae927f4cf22707187409bed582
Paige Watson <paige.watson@outlook.com>
2018-11-29 10:20:39 -0800

    Remove parameters from Console.Write call

--- PowerSetCalculator/ConsoleWrapper.cs
+++ PowerSetCalculator/ConsoleWrapper.cs
@@ -4,7 +4,7 @@ namespace PowerSetCalculator
 {
     public class ConsoleWrapper : IConsoleWrapper
     {
-        public void Write(string format, params object[] parameters) => Console.Write(format, parameters);
-        public void WriteLine(string format, params object[] parameters) => Console.WriteLine(format,parameters);
+        public void Write(string format, params object[] parameters) => Console.Write(format);
+        public void WriteLine(string format, params object[] parameters) => Console.WriteLine(format);
     }
 }
